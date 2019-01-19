a241ae7c6d6190e7597beafa79c2b6e43927bafd
suhasratil <patil.suhas23@gmail.com>
2019-01-20 00:48:41 +0100

    Install latest version of nodejs

--- Assignments/my_default.pp
+++ Assignments/my_default.pp
@@ -1,12 +1,29 @@
 node 'appserver' {
-exec { "apt-get update":
 
-  path => "/usr/bin",
-
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
 }
+
+#exec {"install-curl":
+#command => "sudo apt-get install curl",
+#path => "/usr/bin/:/usr/local/bin/:/bin/"
+#}
+
+package { "curl":
+
+  ensure  => present,
+
+  require => Exec["apt-get update"],
+}
+
+exec { "getcurl":
+require => Package["curl"],
+command => "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -",
+}
+
 package { 'nodejs':
-    ensure              => present,
-    require => Exec["apt-get update"],
+    ensure  => present,
+    require => Exec["getcurl"],
   }
 
 package {"npm":
@@ -14,26 +31,24 @@ package {"npm":
 ensure => present,
  require => Exec["apt-get update"],
 }
-}
 
+}
 
 
 node 'dbserver' {
 
 exec { "apt-get update":
-
   path => "/usr/bin",
-
 }
-
 
   package { "mysql-server":
-
         ensure => installed,
         require => Exec["apt-get update"],
-
 }
 
+service { 'mysql':
+  ensure => running,
+}
 }
 
 
